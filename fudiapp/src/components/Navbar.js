741262ec5d0ad0e8a12926c9a40b564876b7f2df
RupamGanguly46 <rupam.ganguly46@gmail.com>
2026-03-05 13:14:24 +0530

    Different navbars for logged and unlogged user implemented using authToken in localStorage and ternary operators

--- fudiapp/src/components/Navbar.js
+++ fudiapp/src/components/Navbar.js
@@ -1,45 +1,54 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
-  return (
-    <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <div className="container-fluid">
-                <Link className="navbar-brand fs-1 fst-italic" to="/">FUDI</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="#">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/signup">Signup</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/login">Login</Link>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </Link>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><Link className="dropdown-item" to="#">Action</Link></li>
-                        <li><Link className="dropdown-item" to="#">Another action</Link></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-                    </ul>
-                    </li>
-                </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem("authToken");
+        // navigate("/");
+    }
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+                <div className="container-fluid">
+                    <Link className="navbar-brand fs-1 fst-italic" to="/">FUDI</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+                            { (localStorage.getItem("authToken"))
+                            ?
+                                <Link className="nav-link" aria-current="page" to="/">My Orders</Link>
+
+                            : 
+                                ""
+
+                            }
+                        </ul>
+                        { (!localStorage.getItem("authToken")) 
+                        ?
+                            <div className='d-flex'>
+                                <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
+                                <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
+                            </div>
+                        :   
+                            <div className='d-flex'>
+                                <div>
+                                    <Link className="btn bg-white text-success mx-1" to="/">Cart</Link>
+                                </div>
+                                <div>
+                                    <Link className="btn bg-white text-danger mx-1" to="/login" onClick={handleLogout}>Logout</Link>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
-  )
+            </nav>
+        </div>
+    )
 }
